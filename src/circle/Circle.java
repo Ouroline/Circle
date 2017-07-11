570eebb07c0de58097912f7559662c66aac2ad63
mikedaniel <nanomachin3@gmail.com>
2017-07-11 22:38:53 +0700

    area+circumference calculation added

--- src/circle/Circle.java
+++ src/circle/Circle.java
@@ -1,13 +1,28 @@
 package circle;
 public class Circle {
     private double radius;
-    private char color;
+    private String color;
     
     public double getRadius(){
         return radius;
     }
     
-    public char getColor(){
+    public String getColor(){
+        return color;
+    }
+    
+    public void setRadius(double rad){
+        if(rad<=0)
+        {
+            System.out.println("radius cannot be lower than 0");
+        }
+        else
+        {
+            radius = rad;
+        }
+    }
+    
+    public String setColor(){
         return color;
     }
     
@@ -15,14 +30,13 @@ public class Circle {
         return radius * radius * Math.PI;
     }
     
-    public double setRadius(){
-        return radius;
-    }
+    public double getCircumference() {
+      return 2.0 * radius * Math.PI;
+   }
     
-    public char setColor(){
-        return color;
-    }
-    
-    
-    
+    @Override
+    public String toString() {
+      return "Circle[radius=" + radius + ", color=" + color + "]";
+   }
+ 
 }
--- src/circle/Test.java
+++ src/circle/Test.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package circle;
 
-/**
- *
- * @author lucif
- */
+import java.util.Scanner;
+
 public class Test {
+    public static void main(String[] args)
+    {
+    double area;
+    Scanner sc = new Scanner(System.in);
+    Circle c1 = new Circle();
     
+   double rad = sc.nextDouble();
+    
+    c1.setRadius(rad);
+    
+    System.out.println("The Radius is: " + c1.getRadius());
+    System.out.println("The Color is: " + c1.getColor());
+    System.out.println("The Area is: " + c1.getArea());
+    System.out.println("The circumference is: " + c1.getCircumference());
+    }
 }
